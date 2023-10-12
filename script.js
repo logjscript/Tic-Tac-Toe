e2f9bc9d4559757097acde5d5ae04d44e70eb70c
Logan Dietel <logandietel1@gmail.com>
2023-10-11 23:33:52 -0700

    Add gameplay module and player factory function

--- script.js
+++ script.js
@@ -1,28 +1,101 @@
+//Gameboard Setup
 let gameboard = {
     board: [],
-    boardTiles: function () {
-        for (let i = 0; i < 9; i++) {
-            const newListItem = document.createElement('li');
-            newListItem.classList.add('tile');
-            newListItem.id = 'tileNumber' + [i];
-            newListItem.textContent = '';
 
+    init: function() {
+        this.cacheDom();
+        players.createPlayer("Logan", "X");
+        players.createPlayer("Bethany", "O");
+        this.addBoardTiles();
+        this.render();
+        console.log(gameplay.clicks)
+        gameplay.playerSelection();
+    },
+
+    addBoardTiles: function () {
+        for (let i = 0; i < 9; i++) {
             let tile = {
                 clicked: false,
-                tileRef: newListItem,
+                text: '',
+                boardRef: null,
             };
-            this.ul.appendChild(newListItem);
             this.board.push(tile);
         }
-        this.cacheDom();
         return this.board;
     },
+    
     cacheDom: function() {
         this.gameboardContainer = document.getElementById('gameboardContainer');
         this.ul = this.gameboardContainer.querySelector('#gameboardTiles');
         this.tileList = this.ul.querySelectorAll('.tile');
     },
+
+    render: function() {
+        while (this.ul.firstChild) {
+            this.ul.removeChild(this.ul.firstChild);
+        }
+
+        this.board.forEach(tile => {
+            const newListItem = document.createElement('li');
+            newListItem.classList.add('tile');
+            newListItem.textContent = tile.text;
+            this.ul.appendChild(newListItem);
+
+            tile.boardRef = newListItem;
+
+            if (tile.clicked !== true) {
+                this.bindEvents(newListItem);
+            }
+        });
+    },
+
+    bindEvents: function(emptyTile) {
+        emptyTile.addEventListener('click', gameplay.markTile.bind(gameplay));
+    },
 }
 
-gameboard.cacheDom();
-gameboard.boardTiles();
+//Player Creation
+let players = {
+    playerList: [],
+
+    createPlayer: function(name, markerType) {
+        const player = {
+            name: name, 
+            markerType: markerType,
+        }
+        this.playerList.push(player);
+    },
+
+}
+
+
+//Gameplay
+let gameplay = {
+    clicks: 0,
+
+    markTile: function(event) {
+        const clickedTile = event.target;
+        const tile = gameboard.board.find(tile => tile.boardRef === clickedTile);
+        const player = this.playerSelection(); 
+
+        tile.text = player.markerType;
+        clickedTile.textContent = tile.text;
+        tile.clicked = true;
+        this.clicks++;
+    },
+
+    playerSelection: function() {
+        if (this.clicks % 2 === 0) {
+            return players.playerList[0];
+        } else {
+            return players.playerList[1];
+        }
+    },
+
+    playTurn: function() {
+        
+        gameboard.render();
+    },
+}
+
+gameboard.init();
